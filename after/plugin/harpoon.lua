87eff4676537de42a8bd9ca9a49854198658bdef
David Cortes <david.cortesmantilla@mercadolibre.com.co>
2023-10-19 15:46:50 -0500

    Update harpoon.lua

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,5 +5,5 @@ vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
 
 
-vim.keymap.set("n", "<C-k>", function() ui.nav_next() end) 
-vim.keymap.set("n", "<C-j>", function() ui.nav_prev() end) 
+vim.keymap.set("n", "<C-p>", function() ui.nav_next() end) 
+vim.keymap.set("n", "<C-o>", function() ui.nav_prev() end) 
