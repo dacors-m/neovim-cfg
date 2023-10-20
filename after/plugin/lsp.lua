2342b61d460347de50f4473c1ed709d0a1049930
William David Cortes Mantilla <davidcortes@clubadsoft.com>
2023-10-19 19:31:32 -0500

    awesome changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,3 +8,12 @@ lsp.setup()
 
 vim.keymap.set("n", "<leader>rv", function() vim.lsp.buf.rename() end)
 vim.keymap.set("n", "<leader>;", function() vim.lsp.buf.hover() end)
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }
+})
+
