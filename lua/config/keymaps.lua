cee2ac863cc3fb94fb7b4e1bd22618d5a0f6985c
dcortesmanti_meli <david.cortesmantilla@mercadolibre.com.co>
2023-11-15 12:20:03 -0500

    2024 nvim config start

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,30 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "<leader>w", vim.cmd.w)
+keymap.set("n", "<leader>q", vim.cmd.q)
+
+--move code lines
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+
+--tmux move
+keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
+keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
+keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>")
+keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>")
+
+--lsp
+keymap.set("n", "<leader>;", function()
+  vim.lsp.buf.hover()
+end)
+
+--select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+--fugitive
+keymap.set("n", "<leader>gg", vim.cmd.Git)
---
+++ lua/plugins/netrew.lua
@@ -0,0 +1,3 @@
+vim.g.netrw_bufsettings = "nu"
+vim.g.netrw_banner = 0
+return {}
