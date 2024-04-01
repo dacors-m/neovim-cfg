352aaee62b3d1de948eedbdc255a55c86584c154
dcortesmanti_meli <david.cortesmantilla@mercadolibre.com.co>
2024-04-01 14:03:43 -0500

    finish this poc

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,23 @@ return {
     "catppuccin/nvim",
     as = "catppuccin",
     lazy = false,
-    config = function()
+    init = function()
       local theme = require("catppuccin")
       theme.setup({
         flavour = "mocha", -- latte, frappe, macchiato, mocha
+        compile = {
+          enable = true,
+          compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+          suffix = "_compiled",
+        },
+        color_overrides = {
+          mocha = {
+            base = "#11111b",
+          },
+        },
         custom_highlights = function(colors)
           return {
-            WinSeparator = { fg = colors.blue },
+            WinSeparator = { fg = colors.lavender },
           }
         end,
       })
@@ -20,12 +30,12 @@ return {
     "folke/tokyonight.nvim",
     as = "tokyonight",
     lazy = false,
-    config = function()
+    init = function()
       local theme = require("tokyonight")
       theme.setup({
         style = "night", -- "day" or "night"
         on_highlights = function(hl, c)
-          hl.WinSeparator = { fg = c.cyan, bg = c.blue }
+          hl.WinSeparator = { fg = c.blue }
         end,
       })
     end,
--- lua/plugins/netrew.lua
+++
@@ -1,3 +0,0 @@
-vim.g.netrw_bufsettings = "nu"
-vim.g.netrw_banner = 0
-return {}
